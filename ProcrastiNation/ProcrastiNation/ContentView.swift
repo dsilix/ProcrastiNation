ee08e76aa309dfada8241aefdcae474c0244c210
Radalienpunks <Radalienpunks@none.com>
2021-11-16 16:52:12 +0100

    prova 2

--- ProcrastiNation/ProcrastiNation/ContentView.swift
+++ ProcrastiNation/ProcrastiNation/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
         let id = UUID()
     }
     private var oceans = [
-        Task(name: "Prova1"),
+        Task(name: "Prova2"),
         Task(name: "Read the 13/11 search"),
         Task(name: "Start Latin"),
         Task(name: "Studying the 9/11 workshop"),
